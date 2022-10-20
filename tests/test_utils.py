8174e8e198fe207cdc6fc6fc61e4b032defe6796
Chad Crawford <chadcr@amazon.com>
2022-10-20 14:13:45 -0700

    fix: Cleanup test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,19 +2,19 @@ from poetry_plugin_pypi_proxy.utils import parse_url
 
 
 def test_parse_url():
-    input = [
+    input_urls = [
         "http://proxy.org/pypi/simple",
         "http://proxy.org/pypi/simple/",
         "http://proxy.org/pypi/simple/simple",
         "http://proxy.org/pypi/simple/simple/",
     ]
-    expected = [
+    expected_urls = [
         "http://proxy.org/pypi/",
         "http://proxy.org/pypi/",
         "http://proxy.org/pypi/simple/",
         "http://proxy.org/pypi/simple/",
     ]
 
-    for index in range(len(input)):
-        actual = parse_url(input[index])
-        assert actual == expected[index]
+    for input_url, expected_url in zip(input_urls, expected_urls):
+        actual_url = parse_url(input_url)
+        assert actual_url == expected_url
