b3d348027df5d97238560038a0361e3462136c73
Chad Crawford <chad@cacrawford.org>
2022-10-26 15:59:53 -0700

    fix: cleanup test_utils (#5)
    Minor cleanup to avoid call to `range(len())`. Fixes a previous issue
    with the code that would lead to an error when the `simple` postfix was
    missing.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,20 +1,32 @@
+from __future__ import annotations
+
+import pytest
+
 from poetry_plugin_pypi_proxy.utils import parse_url
 
 
-def test_parse_url():
-    input = [
-        "http://proxy.org/pypi/simple",
-        "http://proxy.org/pypi/simple/",
-        "http://proxy.org/pypi/simple/simple",
-        "http://proxy.org/pypi/simple/simple/",
-    ]
-    expected = [
-        "http://proxy.org/pypi/",
-        "http://proxy.org/pypi/",
-        "http://proxy.org/pypi/simple/",
-        "http://proxy.org/pypi/simple/",
-    ]
-
-    for index in range(len(input)):
-        actual = parse_url(input[index])
-        assert actual == expected[index]
+@pytest.mark.parametrize(
+    ["input_urls", "expected_url"],
+    [
+        (
+            [
+                "http://proxy.org/pypi",
+                "http://proxy.org/pypi/",
+                "http://proxy.org/pypi/simple",
+                "http://proxy.org/pypi/simple/",
+            ],
+            "http://proxy.org/pypi",
+        ),
+        (
+            [
+                "http://proxy.org/pypi/simple/simple",
+                "http://proxy.org/pypi/simple/simple/",
+            ],
+            "http://proxy.org/pypi/simple",
+        ),
+    ],
+)
+def test_parse_url(input_urls: list[str], expected_url: str) -> None:
+    for input_url in input_urls:
+        actual_url = parse_url(input_url)
+        assert actual_url == expected_url
